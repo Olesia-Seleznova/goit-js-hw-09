231c4b0402d9951004173b1a09ef41e5e85b93ef
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2022-11-04 19:53:28 +0200

    add 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
 
+let getEl = selector => document.querySelector(selector);
+
+const body = document.querySelector('body');
+getEl('[data-start]').addEventListener('click', btnStartClick);
+getEl('[data-stop]').addEventListener('click', btnStoptClick);;
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function btnStartClick() {
+    interval = setInterval(() => {
+       body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function btnStoptClick() {
+    clearInterval(interval);
+}
