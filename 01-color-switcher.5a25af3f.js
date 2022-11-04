33eb4903cad180ab8e17f96d84d4382168f72542
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2022-11-04 18:45:29 +0000

    Deploying to gh-pages from @ Olesia-Seleznova/goit-js-hw-09@c9cd415a395407c21e2172df943067072f66817b 🚀

--- 01-color-switcher.5a25af3f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=function(t){return document.querySelector(t)},n=document.querySelector("body");t("[data-start]").addEventListener("click",(function(){e=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t("[data-stop]").addEventListener("click",(function(){clearInterval(e)}));var e=null}();
-//# sourceMappingURL=01-color-switcher.5a25af3f.js.map
--- 01-color-switcher.9464a480.js
+++
@@ -1,2 +0,0 @@
-let t=t=>document.querySelector(t);const e=document.querySelector("body");t("[data-start]").addEventListener("click",(function(){n=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t("[data-stop]").addEventListener("click",(function(){clearInterval(n)}));let n=null;
-//# sourceMappingURL=01-color-switcher.9464a480.js.map
