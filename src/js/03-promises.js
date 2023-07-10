2efeee24e0bdc301ffee83e6e47bc34e9ee9560a
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2023-07-10 19:41:12 +0300

    Update 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,24 @@
 import Notiflix from 'notiflix';
 
-const refs = {
-  form: document.querySelector('.form'),
-  delay: document.querySelector('[name="delay"]'),
-  step: document.querySelector('[name="step"]'),
-  amount: document.querySelector('[name="amount"]'),
-};
-
+const formRef =  document.querySelector('.form');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-   setTimeout(() => {
+    setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
     }, delay);
-  })
+  });
+};
+
+const promisificator = (delay, step, amount) => {
+  let stepDelay = 0;
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, stepDelay)
     .then(({ position, delay }) => {
      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
      return { position, delay };
@@ -26,17 +26,11 @@ function createPromise(position, delay) {
     .catch(({ position, delay }) => {
       Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
-};
-
-const promisificator = (delay, step, amount) => {
-  let stepDelay = 0;
-  for (let i = 1; i <= amount; i += 1) {
     stepDelay += step;
-    createPromise(i, stepDelay);
   };
 };
 
-refs.form.addEventListener('submit', onSubmit);
+formRef.addEventListener('submit', onSubmit);
 
 function onSubmit (evt) {
   evt.preventDefault();
